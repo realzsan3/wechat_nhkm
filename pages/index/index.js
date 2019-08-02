dbb85d98a30574a00397d8c6c764ecb5ec561a99
yong-s <jaime.chonger@gmail.com>
2019-08-02 20:26:55 +0800

    update new

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,71 +1,108 @@
-Page({
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    // https://picsum.photos/1080/1920/?image=302
-    image: '/assets/background.jpg',
-    result: null
-  },
+ Page({
+   /**
+    * 页面的初始数据
+    */
+   data: {
+     image: 'https://i.loli.net/2019/07/06/5d20528fd2bba31591.jpg',
+     yiyan: {}
+   },
 
-  detectImage(src) {
-    wx.showLoading({ title: '分析中...' })
+   detectImage(src) {
+     wx.showLoading({
+       title: '分析中...'
+     })
 
-    const that = this
+     const that = this
 
-    wx.uploadFile({
-      url: 'https://ai.qq.com/cgi-bin/appdemo_detectface',
-      filePath: src,
-      name: 'image_file',
-      success(res) {
-        const result = JSON.parse(res.data)
+     wx.uploadFile({
+       url: 'https://ai.qq.com/cgi-bin/appdemo_detectface',
+       filePath: src,
+       name: 'image_file',
+       success(res) {
+         console.log(res);
+         const result = JSON.parse(res.data)
 
-        // 检测失败
-        if (result.ret !== 0) {
-          wx.showToast({ icon: 'none', title: '找不到你的小脸蛋喽' })
-          return false
-        }
+         // 检测失败
+         if (result.ret !== 0) {
+           wx.showToast({
+             icon: 'none',
+             title: '找不到你的小脸蛋喽'
+           })
+           return false
+         }
 
-        that.setData({ result: result.data.face[0] })
-        wx.hideLoading()
-      }
-    })
-  },
+         that.setData({
+           result: result.data.face[0]
+         })
+         wx.hideLoading()
+       }
+     })
+   },
 
-  getImage(type = 'camera') {
-    const that = this
-    wx.chooseImage({
-      sizeType: ['original', 'compressed'],
-      sourceType: [type],
-      success(res) {
-        const image = res.tempFiles[0]
+   getImage(type = 'camera') {
+     const that = this
+     wx.chooseImage({
+       count: 1,
+       sizeType: ['original', 'compressed'],
+       sourceType: [type],
+       success(res) {
+         const image = res.tempFiles[0]
 
-        // 图片过大
-        if (image.size > 1024 * 1000) {
-          wx.showToast({ icon: 'none', title: '图片过大, 请重新拍张小的！' })
-          return false
-        }
+         // 图片过大
+         if (image.size > 1024 * 1000) {
+           wx.showToast({
+             icon: 'none',
+             title: '图片过大, 请重新拍张小的！'
+           })
+           return false
+         }
 
-        that.setData({ image: image.path })
-        that.detectImage(image.path)
-      }
-    })
-  },
+         that.setData({
+           image: image.path
+         })
+         that.detectImage(image.path)
+       }
+     })
+   },
 
-  handleCamera() {
-    this.getImage()
-  },
+   handleCamera() {
+     this.getImage();
+   },
 
-  handleChoose() {
-    this.getImage('album')
-  },
+   handleChoose() {
+     this.getImage('album');
+   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-    if (this.data.result) {
-      return { title: `刚刚测了自己的颜值为【${this.data.result.beauty}】你也赶紧来试试吧！` }
-    }
-  }
-})
+   getYiyan() {
+     var that = this
+     wx.request({
+       url: 'https://v1.hitokoto.cn/',
+       header: {
+         'content-type': 'application/json' // 默认值
+       },
+       success(res) {
+         console.log(res.data);
+         that.setData({
+           yiyan: res.data.hitokoto
+         })
+       }
+     })
+   },
+   /**
+    * 生命周期函数--监听页面加载
+    */
+   onLoad: function(options) {
+     this.getYiyan();
+   },
+
+   /**
+    * 用户点击右上角分享
+    */
+   onShareAppMessage() {
+     if (this.data.result) {
+       return {
+         title: `刚刚测了自己的颜值为【${this.data.result.beauty}】你也赶紧来试试吧！`
+       }
+     }
+   }
+ })
